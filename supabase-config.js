dbb679582f57b6e42031c2e4d48bddf9917eeabb
robexter <139023401+robexter@users.noreply.github.com>
2026-09-08 22:14:59 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-// Mantido somente por compatibilidade.
-export const SUPABASE_URL = "https://gsaxsfghhjuhltnnwcvf.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_OYybjNMQ1gzu_R8gw_9NGg_vsZBxPPM";
